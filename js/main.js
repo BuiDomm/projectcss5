a2f4e03bf05868ab793bfcf030fb5f2fadb7c38d
BuiDomm <114859105+BuiDomm@users.noreply.github.com>
2023-08-29 19:53:55 +0700

    autoChangeSlide

--- js/main.js
+++ js/main.js
@@ -1,19 +1,56 @@
-var sum = 1;
+window.addEventListener('load', function () {
+    const listSlider = document.querySelector('.quotes--list');
+    const mainSlider = document.querySelector('.quotes--main');
+    const slider = document.querySelector('.quotes--item');
+    const dots = document.querySelectorAll('.quotes--dots-item')
+    const coupleItems = document.querySelectorAll('.quotes--couple')
+    console.log([listSlider, mainSlider, slider, dots, coupleItems])
+    let positionX = 0;
+    const widthOfCouple = coupleItems[0].offsetWidth;
+    const dotsArray = [...dots];
 
-var listUsers = [{ name: 'Bui Nhan', age: 20, salary: 100 },
-{ name: 'Hgiang', age: 90, salary: 300 },
-{ name: 'NhiLe', age: 30, salary: 250 },
-{ name: 'VjetDuong', age: 26, salary: 80 }
-]
+    let positionCheck = widthOfCouple * coupleItems.length;
+    let index = 1;
+    console.log(positionCheck);
+    function handleChangeSlide() {
+        positionX = positionX - widthOfCouple;
+        console.log(positionX);
+        if (-positionCheck >= positionX) {
+            positionX = 0;
+        }
 
-listUsers.map((user, index) => {
-    if (user.salary > 100) {
-        sum = sum + 1;
     }
-}
-)
 
-console.log(sum)
+    function handleAutoChangeSlide() {
+        setInterval(() => {
+            handleChangeSlide();
+            index++;
+            if (index > 3) index = 1;
+            console.log(index)
+            mainSlider.style = `transform: translateX(${positionX}px)`;
+            const checkActive = document.querySelector('.quotes--dots-item.active');
+            if (checkActive) {
+                checkActive.classList.remove('active');
+            }
+            dotsArray[index - 1].classList.add('active')
+        }, 8000)
+    }
+    dotsArray.forEach((dot) => {
+        dot.addEventListener('click', function (e) {
+            const dotIndex = parseInt(e.target.dataset.index)
+            const checkActive = document.querySelector('.quotes--dots-item.active');
+            if (checkActive) {
+                checkActive.classList.remove('active');
+            }
+
+            this.classList.add('active')
+            mainSlider.style = `transform: translateX(${-1 * (dotIndex - 1) * widthOfCouple}px)`;
 
 
+        })
+    })
 
+    handleAutoChangeSlide();
+
+
+})
